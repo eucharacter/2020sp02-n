e33f6f63accd16a8d11d9bc18fb2a7e393b3fefc
eucharacter <euca.fun@gmail.com>
2020-07-09 19:30:28 +0800

    默认更改列表

--- 2020sp02/wsgi.py
+++ 2020sp02/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for summerInternProjectBackend project.
+WSGI config for 2020sp02 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'summerInternProjectBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '2020sp02.settings')
 
 application = get_wsgi_application()
